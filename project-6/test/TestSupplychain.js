ae71cea78bf0dd8db648e27ee2818dd37086883e
Fernanda Brasil <69606011+dooapps@users.noreply.github.com>
2021-08-21 20:06:18 -0300

    Update TestSupplychain.js

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -73,7 +73,8 @@ contract('SupplyChain', function(accounts) {
         })
 
         // Mark an item as Harvested by calling function harvestItem()
-        await supplyChain.harvestItem(upc, originFarmerID, originFarmName, originFarmInformation, originFarmLatitude, originFarmLongitude, productNotes)
+        await supplyChain.harvestItem.sendTransaction(upc, originFarmerID, originFarmName, originFarmInformation, originFarmLatitude, originFarmLongitude, productNotes)
+
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
         const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc)
